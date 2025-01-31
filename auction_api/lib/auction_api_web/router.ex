b862dc6c102b99067b5c33163c94fc82f0471861
Everett Griffiths <everett@fireproofsocks.com>
2025-01-31 06:51:22 -0500

    Generated user-auth via phx.gen.auth; run migrations, update deps. Added plug(:fetch_current_user) to the router

--- auction_api/lib/auction_api_web/router.ex
+++ auction_api/lib/auction_api_web/router.ex
@@ -1,6 +1,8 @@
 defmodule AuctionApiWeb.Router do
   use AuctionApiWeb, :router
 
+  import AuctionApiWeb.UserAuth
+
   pipeline :browser do
     plug(:accepts, ["html"])
     plug(:fetch_session)
@@ -8,6 +10,7 @@ defmodule AuctionApiWeb.Router do
     plug(:put_root_layout, html: {AuctionApiWeb.Layouts, :root})
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
+    plug(:fetch_current_user)
   end
 
   pipeline :api do
@@ -18,8 +21,6 @@ defmodule AuctionApiWeb.Router do
     pipe_through(:browser)
 
     get("/", PageController, :home)
-    # get("/auctions", PageController, :auctions)
-    # live("/switch", LightLive)
   end
 
   scope "/auctions", AuctionApiWeb do
@@ -60,6 +61,44 @@ defmodule AuctionApiWeb.Router do
 
       live_dashboard("/dashboard", metrics: AuctionApiWeb.Telemetry)
       forward("/mailbox", Plug.Swoosh.MailboxPreview)
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", AuctionApiWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{AuctionApiWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", AuctionApiWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{AuctionApiWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", AuctionApiWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{AuctionApiWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
